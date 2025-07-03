778ebf2e2015aa565d92c19c58f330428b885214
seth1nk <kostik.vs10@mail.ru>
2025-07-03 23:10:35 +0800

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,24 +4,6 @@ import axios from 'axios';
 import * as VKID from '@vkid/sdk';
 import vkidIcon from '../assets/img/nav-icon1.svg';
 
-// Функция для генерации code_verifier
-const generateCodeVerifier = () => {
-  const array = new Uint8Array(32);
-  crypto.getRandomValues(array);
-  return Array.from(array, (byte) => byte.toString(16).padStart(2, '0')).join('');
-};
-
-// Функция для генерации code_challenge из code_verifier
-const generateCodeChallenge = async (verifier) => {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(verifier);
-  const digest = await crypto.subtle.digest('SHA-256', data);
-  return btoa(String.fromCharCode(...new Uint8Array(digest)))
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_')
-    .replace(/=+$/, '');
-};
-
 const APP_NAME = 'VKAPITEST';
 const CLIENT_ID = '53544787';
 const REDIRECT_URI = 'https://react-lime-delta.vercel.app';
@@ -42,34 +24,22 @@ const Login = ({
   const [name, setName] = useState('');
   const [error, setError] = useState('');
 
-  // Инициализация VKID SDK с PKCE
   useEffect(() => {
     console.log('Login.js: VKID SDK imported:', VKID);
-    const initializeVKID = async () => {
-      try {
-        const codeVerifier = generateCodeVerifier();
-        const codeChallenge = await generateCodeChallenge(codeVerifier);
-        localStorage.setItem('vk_code_verifier', codeVerifier);
-
-        VKID.Config.init({
-          app: Number(CLIENT_ID), // Убедитесь, что app - число
-          redirectUri: REDIRECT_URI, // Исправлено с redirectUrl на redirectUri
-          state: 'state123',
-          codeChallenge,
-          codeChallengeMethod: 'S256',
-          scope: 'email',
-          statsFlow: false, // Отключаем отправку статистики для обхода CORS
-        });
-        console.log('Login.js: VKID initialized successfully with PKCE');
-      } catch (err) {
-        console.error('Login.js: Ошибка инициализации VKID:', err);
-        setError('Ошибка инициализации VKID: ' + err.message);
-      }
-    };
-    initializeVKID();
+    try {
+      VKID.Config.init({
+        app: CLIENT_ID,
+        redirectUrl: REDIRECT_URI,
+        state: 'state123',
+        scope: 'email',
+      });
+      console.log('Login.js: VKID initialized successfully');
+    } catch (err) {
+      console.error('Login.js: Ошибка инициализации VKID:', err);
+      setError('Ошибка инициализации VKID');
+    }
   }, []);
 
-  // Проверка токена при загрузке
   useEffect(() => {
     const googleToken = localStorage.getItem('google_access_token');
     const vkToken = localStorage.getItem('vk_access_token');
@@ -200,10 +170,7 @@ const Login = ({
       })
         .on(VKID.WidgetEvents.ERROR, (error) => {
           console.error('Login.js: Ошибка VKID:', error);
-          setError(`Ошибка VKID: ${error.text || 'Неизвестная ошибка'}`);
-        })
-        .on(VKID.WidgetEvents.SUCCESS, (data) => {
-          console.log('Login.js: Успешная авторизация VKID:', data);
+          setError('Ошибка при авторизации через VKID');
         });
       console.log('Login.js: Кнопка VKID отрендерена');
     } catch (error) {
@@ -341,7 +308,7 @@ const Login = ({
             </div>
             <div className="inputForm">
               <svg height="20" viewBox="-64 0 512 512" width="20" xmlns="http://www.w3.org/2000/svg">
-                <path d="m336 512h-288c-26.453125 0-48-21.523438-48-48v-224c0-26.476562 21.546875-48 48-48h288c26.453125 0 48 21.523438 48 48v224c0 26.476562-21.546875 48-48 48zm-288-288c-8.8125 0-16 7.167969-16 16v224c0 8.832031 7.1875 16 16 16h288c8.8125 0 16-7.167969 16-16v-224c0-8.832031-7.1875-16-16-16zm0 0"></path>
+                <path d="m336 512h0-288c-26.453125 0-48-21.523438-48-48v-224c0-26.476562 21.546875-48 48-48h288c26.453125 0 48 21.523438 48 48v224c0 26.476562-21.546875 48-48 48zm-288-288c-8.8125 0-16 7.167969-16 16v224c0 8.832031 7.1875 16 16 16h288c8.8125 0 16-7.167969 16-16v-224c0-8.832031-7.1875-16-16-16zm0 0"></path>
                 <path d="m304 224c-8.832031 0-16-7.167969-16-16v-80c0-52.929688-43.070312-96-96-96s-96 43.070312-96 96v80c0 8.832031-7.167969 16-16 16s-16-7.167969-16-16v-80c0-70.59375 57.40625-128 128-128s128 57.40625 128 128v80c0 8.832031-7.167969 16-16 16zm0 0"></path>
               </svg>
               <input
@@ -352,7 +319,7 @@ const Login = ({
                 onChange={(e) => setPassword(e.target.value)}
               />
               <svg viewBox="0 0 576 512" height="1em" xmlns="http://www.w3.org/2000/svg">
-                <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"></path>
+                <path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-8-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"></path>
               </svg>
             </div>
             <button className="button-submit" type="submit">
